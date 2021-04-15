91759f9663c8ec2a82296bf424048ac3c366897f
Aleksandra Rybinska <rybinska.aleksandra@gmail.com>
2021-04-15 16:25:42 +0200

    nav fixed

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const burger = document.getElementById("burger");
 const ul = document.querySelector("nav ul");
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
 burger.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', ()=> {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    target.classList.add('active');
+    tab.classList.add('active')
+  })
+})
